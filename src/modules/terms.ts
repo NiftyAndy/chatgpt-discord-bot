f0df69802ce53470ca6919c4d6695b649aee2ff2
NiftyAndy <andy@niftyleague.com>
2023-03-21 15:17:27 -0400

    set port from ENV

--- src/modules/terms.ts
+++ src/modules/terms.ts
@@ -1,28 +1,27 @@
-import supabase from "./supabase.js";
-import fs from "fs";
-import { VoteClient } from "topgg-votes";
+import supabase from './supabase.js';
+import { VoteClient } from 'topgg-votes';
 
 const votesClient = new VoteClient()
   .setToken(process.env.TOPGG_TOKEN)
-  .setPort(randomPort());
+  .setPort(Number(process.env.PORT || 5005));
 votesClient.postWebhook();
 export async function checkTerms(userId, platform) {
   const { data, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("id", userId);
+    .from('users')
+    .select('*')
+    .eq('id', userId);
 
   if (error) {
     console.log(error);
     return false;
   }
   if (!data || data.length == 0 || !data[0]) {
-    await supabase.from("users").insert([
+    await supabase.from('users').insert([
       {
         id: userId,
         platform: platform,
         acceptterms: true,
-        defaultChatModel: "chatgpt",
+        defaultChatModel: 'chatgpt',
       },
     ]);
     return `By using this service you accept the following terms of service:\n\nhttps://turingai.tech/botterms\n\nThis message is going to be deleted in 8s in order to continue with your request.`;
@@ -33,7 +32,7 @@ export async function checkTerms(userId, platform) {
         votesClient.hasVoted(userId),
         new Promise<boolean>((_, reject) => {
           setTimeout(() => {
-            reject(new Error("Timeout"));
+            reject(new Error('Timeout'));
           }, 15000);
         }),
       ]);
@@ -49,23 +48,12 @@ export async function hasVoted(userId) {
       votesClient.hasVoted(userId),
       new Promise<boolean>((_, reject) => {
         setTimeout(() => {
-          reject(new Error("Timeout"));
+          reject(new Error('Timeout'));
         }, 5000);
       }),
     ]);
     return hasVoted;
   } catch (e) {
     return false;
-  }
-}
-
-function randomPort() {
-  let port = Math.floor(Math.random() * (65535 - 1024) + 1024);
-  // check if port is being used
-  try {
-    fs.accessSync(`http://localhost:${port}`);
-    return randomPort();
-  } catch (e) {
-    return port;
   }
 }
